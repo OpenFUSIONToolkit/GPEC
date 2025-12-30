eef74a94a897658d7ff676fee586970d20f93751
Matthew Pharr <m.pharr@protonmail.com>
2025-12-30 12:58:34 -0500

    JPEC - RENAME - fixed some straggler mentions of "DCON"

--- src/ForceFreeStates/ForceFreeStates.jl
+++ src/ForceFreeStates/ForceFreeStates.jl
@@ -16,7 +16,7 @@ using Printf
 import StaticArrays: @MVector, @MMatrix
 
 # Include all necessary files
-include("DconStructs.jl")
+include("ForceFreeStatesStructs.jl")
 include("Mercier.jl")
 include("Ode.jl")
 include("Sing.jl")
