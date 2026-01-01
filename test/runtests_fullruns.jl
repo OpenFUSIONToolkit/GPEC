cc5d471dc72ae1b99e1f8b99fea5452d4ae3cecd
logan-nc <6198372+logan-nc@users.noreply.github.com>
2025-12-31 22:44:29 -0800

    EXAMPLES - MINOR - updates JPEC main syntax

--- test/runtests_fullruns.jl
+++ test/runtests_fullruns.jl
@@ -2,9 +2,9 @@
 @testset "Full ForceFreeStates runs" begin
     ex1 = joinpath(@__DIR__, "test_data", "regression_solovev_ideal_example")
     @info "Running Solovev ideal example"
-    @test isnothing(JPEC.main(ex1))
+    @test isnothing(JPEC.main([ex1]))
 
     ex2 = joinpath(@__DIR__, "test_data", "regression_solovev_ideal_example_multi_n")
     @info "Running Solovev ideal multi-n example"
-    @test isnothing(JPEC.main(ex2))
+    @test isnothing(JPEC.main([ex2]))
 end
