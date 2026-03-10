8438505e357378734c6cb8ec6e47dc634b16e2b6
satelite2517 <satelite2517@gmail.com>
2026-03-10 16:16:31 +0900

    WIP - remove files to re-struct the file structure

--- src/Pentrc/diagnostics.jl
+++
@@ -1,8 +0,0 @@
-"""
-Minimal diagnostics stub for tests.
-Provides diagnose_all() used by pentrc_interface.jl
-"""
-function diagnose_all()
-    # no-op diagnostic placeholder
-    return nothing
-end
